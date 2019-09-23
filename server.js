64cc1995aaf522c8caee416b6c37e049fe8bfb65
John Wunderle <john.wunderle@us.sogeti.com>
2019-09-23 15:51:49 -0400

    backend validator

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 //Connect Database
 connectDB();
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 //create single endpoint
 app.get('/', (req, res) => res.send('API Running'));
 
