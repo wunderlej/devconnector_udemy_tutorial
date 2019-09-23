341391cc9ae05557254b916d4a1ba036bbd0c3ef
John Wunderle <john.wunderle@us.sogeti.com>
2019-09-23 15:07:25 -0400

    routing added

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Database
+connectDB();
+
+//create single endpoint
+app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+
 const PORT = process.env.PORT || 5000;
 
-app.listen();
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
